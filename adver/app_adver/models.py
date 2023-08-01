68671fdf4d750d170ef5f36769fb68fda13caf6e
Ksumaliya <bershakka339@mali.ru>
2023-08-01 18:29:25 +0300

    Задание № 7

--- adver/app_adver/models.py
+++ adver/app_adver/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Advers(models.Model):
+    title = models.CharField("Title", max_length=60)
+    description = models.TextField("description")
+    price = models.DecimalField("price", max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField("date_of_create", auto_now_add=True)
+    updated_At = models.DateTimeField("date_of_change", auto_now=True)
+    is_auction = models.BooleanField("is_Auction", help_text="choose, if it's auction")
+    
+    class Meta:
+        db_table = "Advers"
+
+    def __str__(self):
+        return f"Advers(id={self.id}, title={self.title}, is_auction={self.is_auction})"
